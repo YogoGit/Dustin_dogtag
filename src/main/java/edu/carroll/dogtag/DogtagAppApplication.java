e66c3aae99419a8c105a52a451962099918eb262
dustgard <77804262+dustgard@users.noreply.github.com>
2023-01-27 14:55:34 -0700

    Initial Dog Tag commit

---
+++ src/main/java/edu/carroll/dogtag/DogtagAppApplication.java
@@ -0,0 +1,13 @@
+package edu.carroll.dogtag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogtagAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogtagAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/carroll/dogtag/DogtagAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.carroll.dogtag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogtagAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
